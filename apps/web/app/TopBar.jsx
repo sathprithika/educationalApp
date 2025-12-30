64a6d1212baa250a34ddf2eeb4087b6dd8384095
sathprithika <sathprithika.s2022cse@sece.ac.in>
2025-12-30 14:56:38 +0530

    line spacing modified

--- apps/web/app/TopBar.jsx
+++ apps/web/app/TopBar.jsx
@@ -87,7 +87,7 @@ export default function TopBar() {
           <span
             className={`
     transition-opacity duration-200
-    text-[14px] leading-[35px]
+    text-[14px] leading-none
     text-center
     whitespace-nowrap
     inline-block

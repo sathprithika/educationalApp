2a9f64ecf8d8c4b53a979383ad37a52e40030d10
sathprithika <sathprithika.s2022cse@sece.ac.in>
2025-12-30 11:32:39 +0530

    topbar modified

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
+import { PropsWithChildren } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className="jakarta-font">{children}</body>
